07052c95c5859fd7a95c3afe9b2abcf212b45481
Soha Rahman <studentinsha12@gmail.com>
2017-11-16 05:53:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Add your doToElementsInArray() function here:
 function doToElementsInArray(array,callback) {
   array.forEach(callback);
-  array.forEach(changeCompletely);
 }
 
 // Add your changeCompletely() function here
-function changeCompletely(element) {
-   element='';
+function changeCompletely(element,index,array) {
+   element=Math.floor(Math.random()*100).toString()+` ${element}s!!!`;
   }
+//doToElementsInArray(array,changeCompletely);
