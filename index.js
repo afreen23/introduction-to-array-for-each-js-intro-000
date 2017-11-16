2a2de84817409c4436d32002db58589339d45d69
Soha Rahman <studentinsha12@gmail.com>
2017-11-16 06:00:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Add your doToElementsInArray() function here:
 function doToElementsInArray(array,callback) {
   array.forEach(callback);
+  return array;
 }
 
 // Add your changeCompletely() function here
